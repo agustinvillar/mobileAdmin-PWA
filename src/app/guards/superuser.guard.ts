ffb1e55a7f9e147290a004b3acede5ca25b14a54
Federico Munoz <fedemz88@gmail.com>
2020-11-17 19:49:45 -0300

    clickable list

--- src/app/guards/superuser.guard.ts
+++ src/app/guards/superuser.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanLoad, Router } from '@angular/router';
-import { UserService } from '../services/user/user.service';
 
+import { UserService } from '../services/user/user.service';
 import { AuthGuard } from './auth.guard';
 
 @Injectable({
